e1a77f006679be9db5621fec465331498c102dad
ThomasdevLC <thomas.lecam@gmail.com>
2025-02-06 15:49:30 +0100

    blogpost+ paints repositories + controllers

--- src/Repository/PeintureRepository.php
+++ src/Repository/PeintureRepository.php
@@ -16,20 +16,18 @@ class PeintureRepository extends ServiceEntityRepository
         parent::__construct($registry, Peinture::class);
     }
 
-    //    /**
-    //     * @return Peinture[] Returns an array of Peinture objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+        /**
+         * @return Peinture[] Returns an array of Peinture objects
+         */
+        public function lastTree(): array
+        {
+            return $this->createQueryBuilder('p')
+                ->orderBy('p.id', 'ASC')
+                ->setMaxResults(3)
+                ->getQuery()
+                ->getResult()
+            ;
+        }
 
     //    public function findOneBySomeField($value): ?Peinture
     //    {
